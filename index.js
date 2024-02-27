3592c3e55acb869ba81b132d212ffee4250ec901
Juan-Mansilla <juancruzmansilla460@gmail.com>
2024-02-27 18:05:49 -0300

    Abre
    abre

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import { express } from 'express';
+import express from 'express';
+import ejs from 'ejs';
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
 
-app.listen(3000)
-console.log("escucho fuerte y claro")
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log('__dirname')
+
+app.set('view engine', 'ejs')
+app.set('views',)
+
+app.use(express.static(join(__dirname, 'public')))
+app.get('/', (req, res) => res.render('index.ejs') )
+
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
+
+console.log('hola')
