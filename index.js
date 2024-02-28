45ffb8440fb33647eaa2b4b318a8095cb4b4ea40
Juan-Mansilla <juancruzmansilla460@gmail.com>
2024-02-28 19:19:43 -0300

    inicio de api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import ejs from 'ejs';
-import {dirname, join} from 'path'
+import { dirname, join } from 'path'
 import { fileURLToPath } from 'url';
 
 const app = express();
@@ -12,11 +12,39 @@ app.set('view engine', 'ejs')
 app.set('views',)
 
 app.use(express.static(join(__dirname, 'public')))
-app.get('/', (req, res) => res.render('index.ejs') )
-
+app.get('/', (req, res) => res.render('index.ejs'))
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
 
 console.log('hola')
+
+//Extracion de la API
+
+app.get('/webhook', (req, res) => {
+  let mode = req.query['hub.mode'];
+  let challenge = req.query['hub.challenge'];
+  let token = req.query['hub.verify_token'];
+
+  const mytoken = 'test';
+
+    if (mode && token){
+
+      if (mode === 'suscribe' && token === mytoken){
+        res.status(200).send(challenge);
+      } else{
+          res.status(403);
+        }
+
+    }
+});
+
+app.post('/webhook', (req, res) => {
+
+    let body_param = req.body;
+    
+    console.log(JSON.stringify(body_param, null, 2));
+    
+
+});
