e0bb397445bfb8085a2b6072ed32874d3772f524
Juan-Mansilla <juancruzmansilla460@gmail.com>
2024-03-13 16:04:52 -0300

    Proyecto Validacion de numeros
    Completamente funcional con la web
    https://whapi.cloud/es/

--- index.cjs
+++ index.cjs
@@ -1,64 +1,65 @@
-// Iniciar codigo en la terminal con "npm run dev"
-const express = require('express')
-const bodyParser = require('body-parser')
-const sdk = require('api')('@whapi/v1.7.5#27slv2oltdcs6xr')
+const express = require('express');
+const bodyParser = require('body-parser');
+const sdk = require('api')('@whapi/v1.7.5#27slv2oltdcs6xr');
 
-// En la línea 7 cambiar el token por el nuevo utilizado
-sdk.auth('na2lcOPDAzbP1vb5AIlIZDnDcFDbblyo')
-const app = express()
+// Reemplazar token con el nuevo
+sdk.auth('uEzmJh9j7fFOI7FIVvc349qpQyj3zRMc');
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.post('/validar-numero', (req, res) => {
-  const numero = req.body.numero
-  const numeroAValidar = numero.replace(/[^0-9]/g, '') || ''
+  const numero = req.body.numero;
+  const numeroAValidar = numero.replace(/[^0-9]/g, '') || '';
 
   const checkNum = async num => {
     try {
       const { data } = await sdk.checkPhones({
         blocking: 'no_wait',
         force_check: false,
-        contacts: [num]
-      })
-      return data
+        contacts: [num],
+      });
+      return data;
     } catch (error) {
-      console.error(error)
+      console.error(error);
       res.render('index', {
         error: error.message,
-        resultadoValidacion: 'no hay data'
-      })
+        resultadoValidacion: 'no hay data',
+      });
     }
 
-    return null
-  }
+    return null;
+  };
 
   checkNum(numeroAValidar).then(data => {
     if (data.contacts.length === 0) {
       checkNum(numeroAValidar).then(data2 => {
-        console.log('la verdadera', data2)
+        console.log('la verdadera', data2);
         if (data2.contacts[0].status) {
           res.render('index', {
-            resultadoValidacion: data2.contacts[0].status
-          })
+            resultadoValidacion: data2.contacts[0].status === 'valid' ? "¡Este número posee Whatsapp!" : "¡Este número no posee Whatsapp!",
+            resultadoValido: data2.contacts[0].status === 'valid',
+          });
         }
-      })
+      });
 
-      return
+      return;
     }
 
     res.render('index', {
-      resultadoValidacion: data
-    })
-  })
-})
+      resultadoValidacion: data.contacts[0].status === 'valid' ? "¡Este número posee Whatsapp!" : "¡Este número no posee Whatsapp!",
+      resultadoValido: data.contacts[0].status === 'valid',
+    });
+  });
+});
 
 app.get('/', (req, res) => {
-  res.render('index', { resultadoValidacion: null, error: null })
-})
+  res.render('index', { resultadoValidacion: null, error: null, resultadoValido: null });
+});
 
 app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000')
-})
+  console.log('Servidor escuchando en el puerto 3000');
+});
